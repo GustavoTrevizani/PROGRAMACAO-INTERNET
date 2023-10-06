b7ff095ce11ed15508e242125e156a49be847646
Gustavo <67985470+GustavoTrevizani@users.noreply.github.com>
2023-10-06 09:37:13 -0300

    Add files via upload

--- aula20231002.js
+++ aula20231002.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $("#salvar").click(function(){
         nome = $("#nome").val();
         datanasc = $("#datanasc").val();
-        $("#clientes tbody").append("<tr><td>"+nome+"</td><td>"+datanasc+"</td></tr>")
+        cidade = $("#cidade").val();
+        uf = $("#uf").val();
+        filhos = $("input[name=filhos]:checked").val();
+        $("#clientes tbody").append("<tr><td>"+nome+"</td><td>"+datanasc+"</td><td>"+cidade+"</td><td>"+uf+"</td><td>"+filhos+"<td></tr>")
     })
 }) 

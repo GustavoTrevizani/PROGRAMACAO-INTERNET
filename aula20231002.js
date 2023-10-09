d505b9439d692946d2a487104b3806e64890f499
Gustavo <67985470+GustavoTrevizani@users.noreply.github.com>
2023-10-09 11:20:33 -0300

    Add files via upload

--- aula20231002.js
+++ aula20231002.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
         cidade = $("#cidade").val();
         uf = $("#uf").val();
         filhos = $("input[name=filhos]:checked").val();
-        $("#clientes tbody").append("<tr><td>"+nome+"</td><td>"+datanasc+"</td><td>"+cidade+"</td><td>"+uf+"</td><td>"+filhos+"<td></tr>")
+        $("#clientes tbody").append("<tr><td>"+nome+"</td><td>"+datanasc+"</td><td>"+cidade+"</td><td>"+uf+"</td><td>"+filhos+"<td><td><input type='button' class='excluir' value='Excluir'></td></tr>")
     })
-}) 
+
+    $("#excluir-tudo").click(function(){
+        $("#clientes tbody").empty();
+    })
+
+    $(".excluir").click(function(){
+        alert("OI");
+    })
+})

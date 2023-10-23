b3f5a4a660abde84064eb9864fa5e9191f4360d5
Gustavo <67985470+GustavoTrevizani@users.noreply.github.com>
2023-10-23 11:22:28 -0300

    23/10/23

--- aula20231020.js
+++ aula20231020.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
-   $("#salvar").click(function(){
+    km_total = 0
+    tempo_total = 0
+
+    $("#adicionar").click(function(){
     de = $("#de").val();
-   }) 
+    para = $("#para").val();
+    km = $("#km").val();
+    velocidade = $("#velocidade").val();
+    tempo = km/velocidade;
+    km_total = km_total + parseInt(km);
+    tempo_total = tempo_total + tempo;
+    $("#trajetos tbody").append("<tr><td>"+de+"</td><td>"+para+"</td><td>"+km+"</td><td>"+velocidade+"</td><td>"+tempo.toFixed(2)+"</td><td><input type='button' value='Excluir' class='excluir'></td></tr>");
+    $("#km_total").text(km_total);
+    $("#tempo_total").text(tempo_total);
+    }) 
+
+    $("#trajetos tbody").on("click", ".excluir", function(){
+        km_total = km_total - ?????
+        tempo_total = tempo_total - ?????
+        $(this).closest("tr").remove();
+        $("#km_total").text(km_total);
+        $("#tempo_total").text(tempo_total);
+    })
 })
 
 
-
-/*$("#salvar").click(function(){
-    nome = $("#nome").val();
-    datanasc = $("#datanasc").val();
-    cidade = $("#cidade").val();
-    uf = $("#uf").val();
-    filhos = $("input[name=filhos]:checked").val();
-    $("#clientes tbody").append("<tr><td>"+nome+"</td><td>"+datanasc+"</td><td>"+cidade+"</td><td>"+uf+"</td><td>"+filhos+"<td><td><input type='button' class='excluir' value='Excluir'></td></tr>")
-})*/

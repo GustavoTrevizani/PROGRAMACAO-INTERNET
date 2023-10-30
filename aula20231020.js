bdb8cbaa6c905920553583743fe36972a71c3d50
Gustavo <67985470+GustavoTrevizani@users.noreply.github.com>
2023-10-30 10:30:32 -0300

    Add files via upload

--- aula20231020.js
+++ aula20231020.js
@@ -10,14 +10,15 @@ $(document).ready(function(){
     tempo = km/velocidade;
     km_total = km_total + parseInt(km);
     tempo_total = tempo_total + tempo;
-    $("#trajetos tbody").append("<tr><td>"+de+"</td><td>"+para+"</td><td>"+km+"</td><td>"+velocidade+"</td><td>"+tempo.toFixed(2)+"</td><td><input type='button' value='Excluir' class='excluir'></td></tr>");
+    $("#trajetos tbody").append("<tr><td>"+de+"</td><td>"+para+"</td><td class='km'>"+km+"</td><td>"+velocidade+"</td><td class='tempo'>"+tempo.toFixed(2)+"</td><td><input type='button' value='Excluir' class='excluir'></td></tr>");
     $("#km_total").text(km_total);
     $("#tempo_total").text(tempo_total);
     }) 
 
     $("#trajetos tbody").on("click", ".excluir", function(){
-        km_total = km_total - ?????
-        tempo_total = tempo_total - ?????
+        /*km_total = km_total - ?????
+        tempo_total = tempo_total - ?????*/
+
         $(this).closest("tr").remove();
         $("#km_total").text(km_total);
         $("#tempo_total").text(tempo_total);

dd2e6c9e2bd4dbabc77a823bde2bf2e93b4bf851
Gustavo <67985470+GustavoTrevizani@users.noreply.github.com>
2023-11-10 09:38:24 -0300

    Add files via upload

--- aula20231106.js
+++ aula20231106.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
             $(this).closest("tr").remove(); 
         })
     })
+
+    $("#marca").change(function(){
+        marca = $(this).val();
+        $("#veiculos tbody tr").hide(500);
+        $("."+marca).closest("tr").show();
+    })
 })
